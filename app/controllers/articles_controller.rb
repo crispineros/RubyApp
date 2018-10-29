6fd01a078aaf91bb41c26c1f0f4f38a5f10f3a55
rougan <cristian.pineros@endava.com>
2018-10-29 15:59:50 -0500

    13- Vistas parciales

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,8 +3,43 @@ class ArticlesController < ApplicationController
   def index
     @articles=Article.all
   end
-  #GEY /articles/:id
+
+  #GET /articles/:id
   def show
     @article=Article.find(params[:id])
   end
+
+  #GET /articles/new
+  def new
+    @article = Article.new
+  end
+
+  #POST /articles
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  #PUT /articles/:id
+  def update
+
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title,:body)
+  end
+
 end
